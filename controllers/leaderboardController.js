6c3699283506164f7941b5f0010034579111332d
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-05-27 10:06:16 +0530

    refactor: implement score updating logic for existing user

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -2,20 +2,51 @@ const asyncHandler = require("express-async-handler");
 const Leaderboard = require("../models/leaderboard");
 
 exports.add_score = asyncHandler(async (req, res, next) => {
-  const score = new Leaderboard({
-    username: req.body.username,
-    gameId: req.params.gameId,
-    time: req.body.duration,
-  });
+  const gameId = req.params.gameId;
+  const { username, duration } = req.body;
 
-  const result = await score.save();
-  return res.status(201).json({ success: true });
+  if (!gameId) {
+    return res.status(403).json({
+      message: "Game ID is required to add scores",
+    });
+  }
+
+  if (!username || !duration) {
+    return res.status(400).json({
+      message: "Username and duration are required",
+    });
+  }
+
+  // Find existing score for the username and gameId
+  const existingScore = await Leaderboard.findOne({ username, gameId });
+
+  if (existingScore) {
+    // If existing score is found, compare and update if the new score(duration) is lesser
+    if (duration < existingScore.time) {
+      existingScore.time = duration;
+      await existingScore.save();
+      return res.status(200).json({ success: true, message: "Score updated" });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "New score's duration is not lesser than the existing score",
+      });
+    }
+  } else {
+    // If no existing score is found, create a new score entry
+    const newScore = new Leaderboard({
+      username,
+      gameId,
+      time: duration,
+    });
+
+    const result = await newScore.save();
+    return res.status(201).json({ success: true, message: "Score added" });
+  }
 });
 
 exports.get_scores = asyncHandler(async (req, res, next) => {
-  const gameId = req.params.gameId;
-
-  if (!gameId) {
+  if (!req.params.gameId) {
     return res.status(403).json({
       message: "Game ID is required to view scores",
     });
